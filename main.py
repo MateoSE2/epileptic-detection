21d9aba3e59e4b1c0d48ecf8aa757e1625890b61
MateoSesmaUrbano <mateo.sesma.u@gmail.com>
2022-12-02 13:25:12 +0100

    server send

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     if mode == "preprocessing":
         # run the preprocessing
         data_handler = DataHandler(raw_data_folder, window_folder, metadata_folder, second_window_size=1)
-        data_handler.preprocess_data(excepting = ["chb01_raw_eeg_128.npz", "chb22_raw_eeg_128.npz", "chb22_raw_eeg_128.npz"])
+        data_handler.preprocess_data(excepting = ["chb01_raw_eeg_128.npz", "chb22_raw_eeg_128.npz", "chb24_raw_eeg_128.npz"])
         #data_handler.read_raw_data(os.path.join(raw_data_folder, "chb02_raw_eeg_128.parquet"))
     elif mode == "classic_ml":
         # run the classic machine learning
