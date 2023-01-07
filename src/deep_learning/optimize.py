18b0131116b2a5375d07e4b69f07f4b5e93de760
MateoSesmaUrbano <mateo.sesma.u@gmail.com>
2023-01-07 12:47:20 +0100

    optimize all models

--- src/deep_learning/optimize.py
+++ src/deep_learning/optimize.py
@@ -7,9 +7,20 @@ from pytorch_lightning.loggers import WandbLogger
 
 #from tsai.models.TransformerPlus import TransformerPlus
 from tsai.models.FCNPlus import FCNPlus
+from tsai.models.ResNetPlus import ResNetPlus
+from tsai.models.XceptionTimePlus import XceptionTimePlus
+from tsai.models.RNNPlus import GRUPlus, LSTMPlus, RNNPlus
+from tsai.models.TSSequencerPlus import TSSequencerPlus
+from tsai.models.XResNet1dPlus import xresnet1d50_deeperplus
+from tsai.models.InceptionTimePlus import InceptionTimePlus
+from tsai.models.RNN_FCNPlus import MGRU_FCNPlus, MLSTM_FCNPlus, MRNN_FCNPlus
+
+
+
 
 from src.deep_learning.data.datamodule import DataModule
 from src.deep_learning.models.lightning_module import LightningModule
+
 
 import optuna
 
@@ -25,50 +36,49 @@ class HyperparameterOptimization:
 
         # Create datamodule
         BATCH_SIZE = trial.suggest_int("batch_size", 512, 1024)
+        BATCH_SIZE = 2
         print("Batch size:", BATCH_SIZE)
     
         dm = DataModule(self.root_data_dir, batch_size=BATCH_SIZE)
 
         # Choose model
-        #MODEL_NAME = trial.suggest_categorical("model", ["FCNPlus", "ResCNN", "InceptionBlockPlus", "MLP", "TransformerModel", "GRUPlus", "LSTMPlus"])
+        
+        MODEL_NAME = trial.suggest_categorical("model", ["FCNPlus", "ResNetPlus", "XceptionTimePlus", "GRUPlus", "LSTMPlus", "RNNPlus", "TSSequencerPlus", "xresnet1d50_deeperplus", "InceptionTimePlus", "MGRU_FCNPlus", "MLSTM_FCNPlus", "MRNN_FCNPlus"])
+        print("Model:", MODEL_NAME)
+        if MODEL_NAME == "FCNPlus":
+            model = FCNPlus(21, 2)
+        elif MODEL_NAME == "ResNetPlus":
+            model = ResNetPlus(21, 2)
+        elif MODEL_NAME == "XceptionTimePlus":
+            model = XceptionTimePlus(21, 2)
+        elif MODEL_NAME == "GRUPlus":
+            model = GRUPlus(21, 2)
+        elif MODEL_NAME == "LSTMPlus":
+            model = LSTMPlus(21, 2)
+        elif MODEL_NAME == "RNNPlus":
+            model = RNNPlus(21, 2)
+        elif MODEL_NAME == "TSSequencerPlus":
+            model = TSSequencerPlus(21, 2, 128)
+        elif MODEL_NAME == "xresnet1d50_deeperplus":
+            model = xresnet1d50_deeperplus(21, 2)
+        elif MODEL_NAME == "InceptionTimePlus":
+            model = InceptionTimePlus(21, 2)
+        elif MODEL_NAME == "MGRU_FCNPlus":
+            model = MGRU_FCNPlus(21, 2, 128)
+        elif MODEL_NAME == "MLSTM_FCNPlus":
+            model = MLSTM_FCNPlus(21, 2, 128)
+        elif MODEL_NAME == "MRNN_FCNPlus":
+            model = MRNN_FCNPlus(21, 2, 128)
+        else:
+            raise ValueError("Invalid model name.")
 
-        # if MODEL_NAME == "FCNPlus":
-        #     model = FCNPlus(21, 2)
-        # elif MODEL_NAME == "ResCNN":
-        #     model = ResCNN(21, 2)
-        # elif MODEL_NAME == "InceptionBlockPlus":
-        #     model = InceptionBlockPlus(21, 2)
-        # elif MODEL_NAME == "MLP":
-        #     model = MLP(21, 2)
-        # elif MODEL_NAME == "TransformerModel":
-        #     model = TransformerModel(21, 2)
-        # elif MODEL_NAME == "GRUPlus":
-        #     model = GRUPlus(21, 2)
-        # elif MODEL_NAME == "LSTMPlus":
-        #     model = LSTMPlus(21, 2)
-        # else:
-        #     raise ValueError(f"Model {MODEL_NAME} not supported.")
-
-        model = FCNPlus(21, 2)
-        # model = ResNetPlus(21, 2)
-        # model = XceptionTimePlus(21, 2)
-        # model = GRUPlus(21, 2)
-        # model = LSTMPlus(21, 2)
-        # model = RNNPlus(21, 2)
-        # model = TSSequencerPlus(21, 2, 128)
-        # model = xresnet1d50_deeperplus(21, 2)
-        # model = InceptionTimePlus(21, 2)
-        # model = MGRU_FCNPlus(21, 2, 128)
-        # model = MLSTM_FCNPlus(21, 2, 128)
-        # model = MRNN_FCNPlus(21, 2, 128)
-       
         # Create LightningModule
         num_classes = 2
         LEARNING_RATE = trial.suggest_loguniform("learning_rate", 1e-5, 1e-1)
         model = LightningModule(model, num_classes=num_classes, learning_rate=LEARNING_RATE)
 
         # Logger
-        wandb_logger = WandbLogger(project='epileptic-detection', job_type='train')
+        wandb_logger = None # WandbLogger(project='epileptic-detection', job_type='train')
 
         # Callbacks
         callbacks = [
